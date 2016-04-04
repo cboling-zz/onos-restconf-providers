c74c7f7799c9f4ebf0930bfc3b83226b56eb7fcc
cboling <Painter456>
2016-04-03 19:02:22 -0500

    Test RESTCONF device (for XRD and eventually other debugging purposes)

---
+++ tools/testDevice/testDevice.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#
+# Copyright 2015 - 2016 Boling Consulting Solutions, bcsw.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+from flask import Flask, Response
+from xrd import Element, XRD, Link
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "This is a test program to simulate a RESTCONF capable device.<br\>"
+
+
+@app.route('/.well-known/host-meta', methods=['GET'])
+def get_host_meta():
+    """
+    This function services the well-known host-meta XRD data for RESTCONF
+    API root discovery.
+    """
+    xrd_obj = XRD()
+
+    # Add a few extra elements and links before RESTCONF to help make sure
+    # the parsing/XPATH is correct
+
+    xrd_obj.elements.append(Element('hm:Host', 'testDevice'))
+    xrd_obj.links.append(Link(rel='license', href='http://www.apache.org/licenses/LICENSE-2.0'))
+    xrd_obj.links.append(Link(rel='author', href='http://bcsw.net'))
+
+    # Add the link for RESTCONF
+
+    xrd_obj.links.append(Link(rel='restconf', href='top/restconf'))
+
+    # Add some extra links here as well
+
+    xrd_obj.links.append(Link(rel='testPath', href='this/does/not/exist'))
+    xrd_obj.links.append(Link(rel='http://oexchange.org/spec/0.8/rel/resident-target',
+                              type_='application/xrd+xml',
+                              href='http://twitter.com/oexchange.xrd'))
+
+    # Convert to XML, pretty-print it to aid in debugging
+
+    xrd_doc = xrd_obj.to_xml()
+    return Response(xrd_doc.toprettyxml(indent=' '), mimetype='application/xrd+xml')
+
+
+@app.route('/config/reset', methods=['POST'])
+def do_reset():
+    """
+    This url (/config/reset) can be called to reset configurable parameters
+    to their defaults.
+
+    This allows you to run the server once and call various 'config/set/*' pages
+    with variables to alter the behaviour (for a particular test) and then call
+    this to reset items back to normal so you can do more tests.
+    """
+    pass  # TODO: Need to implement
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
