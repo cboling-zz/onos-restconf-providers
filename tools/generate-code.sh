bdda058774fd1f5d99679a8d723891c7932347cc
cboling <Painter456>
2016-04-17 08:54:22 -0500

    Need YIN output to insure we can find all the attributes we need (config being very important)

--- tools/generate-code.sh
+++ tools/generate-code.sh
@@ -12,6 +12,7 @@ RESTCONF_MODULE_OUTDIR=${RESTCONF_MODULE_BASEDIR}/generated-code
 RESTCONF_MODULES="toaster.yang example-jukebox.yang"
 #RESTCONF_MODULES="toaster.yang"
 PYBIND_OPTS="--use-extmethods --build-rpcs"
+PYIN_OPTS="--format=yin --yin-canonical --yin-pretty-strings --trim-yin"
 
 for module in ${RESTCONF_MODULES}
 do
@@ -20,19 +21,29 @@ do
     # PEP 8 says we need all lowercase names and no hyphens or spaces...
 
     pyFile="${module%%.*}.py"
-    outFile=`echo "${pyFile}" | tr " " _ | tr - _ | tr '[:upper:]' '[:lower:]'`
+    xmlFile="${module%%.*}.xml"
+
+    pyOutFile=`echo "${pyFile}" | tr " " _ | tr - _ | tr '[:upper:]' '[:lower:]'`
+    xmlOutFile=`echo "${xmlFile}" | tr " " _ | tr - _ | tr '[:upper:]' '[:lower:]'`
 
     # echo "Output file is ${outFile}"
 
-    outFilePath=${RESTCONF_MODULE_OUTDIR}/${outFile}
+    pyOutFilePath=${RESTCONF_MODULE_OUTDIR}/${pyOutFile}
+    xmlOutFilePath=${RESTCONF_MODULE_OUTDIR}/${xmlOutFile}
 
     # Remove any existing generated output
-    rm -f ${outFile} >/dev/null 2>&1
+    rm -f ${pyOutFilePath} ${xmlOutFilePath} >/dev/null 2>&1
 
-    # Generate the code now
+    # Generate the python code now via pyangbind
     echo "Generating code for '${module}' to file '${pyFile}'"
-    cmd="pyang --plugindir ${PYBINDPLUGIN} -f pybind ${PYBIND_OPTS} -p ${RESTCONF_MODULE_BASEDIR} -o ${outFilePath} ${inFile}"
+    cmd="pyang --plugindir ${PYBINDPLUGIN} -f pybind ${PYBIND_OPTS} -p ${RESTCONF_MODULE_BASEDIR} -o ${pyOutFilePath} ${inFile}"
     echo ${cmd}
     ${cmd}
+
+    # Create a corresponding YIN document as well so we can get access to all fields/attributes
+    cmd2="pyang ${PYIN_OPTS} -p ${RESTCONF_MODULE_BASEDIR} -o ${xmlOutFilePath} ${inFile}"
+    echo ${cmd2}
+    ${cmd2}
+
     echo "------------"
 done
