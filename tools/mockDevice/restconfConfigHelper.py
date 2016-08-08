a9b727b76295534511356e472918291ae6708b32
cboling <Painter456>
2016-08-08 17:44:02 -0500

    Copyright updates

--- tools/mockDevice/restconfConfigHelper.py
+++ tools/mockDevice/restconfConfigHelper.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 - 2016 Boling Consulting Solutions, bcsw.net
+# Copyright 2015-present Boling Consulting Solutions, bcsw.net
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
